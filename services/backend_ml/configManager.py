9ea8de2dcfcb4d699a98375b413414cef80c7843
kosiarn <kosiarn@users.noreply.github.com>
2023-04-03 12:46:24 +0200

    add util function file stub

---
+++ services/backend_ml/configManager.py
@@ -0,0 +1,16 @@
+
+
+def saveModel(name: str):
+    raise NotImplementedError
+
+def importModel(name: str):
+    raise NotImplementedError
+
+def readModelConfig(file_name: str):
+    raise NotImplementedError
+
+def writeConfig(file_name: str):
+    raise NotImplementedError
+
+def flushModelMemoryToFile(file_name: str, loaded_models: dict):
+    raise NotImplementedError
