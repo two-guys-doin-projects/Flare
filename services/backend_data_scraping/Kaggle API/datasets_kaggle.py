aabf2cd719bc26805d09cca6c3b474a87e496218
Filip Walkowicz <fillio00@wp.pl>
2023-04-12 21:10:42 +0200

    added filter of files available to download

--- services/backend_data_scraping/Kaggle API/datasets_kaggle.py
+++ services/backend_data_scraping/Kaggle API/datasets_kaggle.py
@@ -1,21 +1,38 @@
 from init_kaggle import kaggle_api_authentication
 import pandas as pd
-import io
 
 
 def show_datasets(api, ds_name):
     ds_list = api.dataset_list(search=ds_name)
     df_list_of_datasets = pd.DataFrame(ds_list)
-    df_list_of_files = api.dataset_list_files(df_list_of_datasets[0][1].ref)
+    number_of_datasets = len(df_list_of_datasets)
+    df_list_of_files = pd.DataFrame(
+        check_if_dataset_is_available(api, number_of_datasets, df_list_of_datasets),
+        columns=["dataset", "pliki"],
+    )
+
     return df_list_of_datasets, df_list_of_files
+
+
+def check_if_dataset_is_available(api, length, datasets_frame):
+    df_of_available_datasets = []
+    df_of_available_datasets_files = []
+    for item in range(length):
+        df_list_of_files = api.dataset_list_files(datasets_frame[0][item].ref)
+        if "{}" in str(df_list_of_files.files):
+            continue
+        else:
+            df_of_available_datasets.append(
+                [datasets_frame[0][item], df_list_of_files.files]
+            )
+
+    return df_of_available_datasets
 
 
 def download_dataset(api, dataset):
     dataset = api.dataset_download_file(
-            dataset=str(dataset),
-            file_name=str(api.dataset_list_files(dataset.ref))
-        )
-    print(dataset)
+        dataset=str(dataset), file_name=str(api.dataset_list_files(dataset.ref))
+    )
 
 
 if __name__ == "__main__":
@@ -23,6 +40,6 @@ if __name__ == "__main__":
     # place holder
     dataset_name = "air quality"
     list_of_datasets, list_of_files = show_datasets(kaggle_api, dataset_name)
-    print(f"Lista zbiorów danych spod hasła {dataset_name}: \n {list_of_datasets[0][1]}")
-    print(f"Lista możliwych plików do pobrania: {list_of_files.files}")
-    download_dataset(kaggle_api, list_of_datasets[0][1])
+    print(f"Lista zbiorów danych spod hasła {dataset_name}: \n {list_of_datasets}")
+    print(f"Lista możliwych plików do pobrania: \n {list_of_files}")
+    # download_dataset(kaggle_api, list_of_datasets[0][1])
