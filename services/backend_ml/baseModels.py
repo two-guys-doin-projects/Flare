a1e2eb115442b27895ee78635166fc990b145640
kosiarn <kosiarn@users.noreply.github.com>
2023-04-27 15:12:32 +0200

    add basic neural net model

--- services/backend_ml/baseModels.py
+++ services/backend_ml/baseModels.py
@@ -1,8 +1,17 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as func
 
 class neuralNet(nn.Module):
-    def __init__(self, input_size: int, num_layers: int, output_size: int):
+    def __init__(self, input_size: int, hidden_size: int, num_layers: int, output_size: int):
         super().__init__()
-        #TODO finish neural network init
+        net = []
+        net.append(nn.Linear(input_size, hidden_size))
+        for layer in range(num_layers):
+            net.append(nn.Linear(hidden_size, hidden_size))
+        net.append(nn.Linear(hidden_size, output_size))
+        self.architechture = nn.Sequential(*net)
+    def forward(self, x):
+        x = self.architechture(x)
+        y_hat = func.softmax(x, dim=1)
+        return y_hat
