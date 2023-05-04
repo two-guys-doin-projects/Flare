2666f46a14a1cded662aa894df90bf90a4c27cf1
kosiarn <kosiarn@users.noreply.github.com>
2023-05-04 13:45:06 +0200

    switch trainer params to match neural net init

--- services/backend_ml/trainers.py
+++ services/backend_ml/trainers.py
@@ -2,6 +2,6 @@ import baseModels as models
 import torch
 
 class NeuralNetTrainer():
-    def __init__(self, model_path: str, input_size: int, hidden_size: int, num_layers: int, output_size: int):
-        self.model = models.neuralNet(input_size, hidden_size, num_layers, output_size)
+    def __init__(self, model_path: str, param_dict: dict):
+        self.model = models.neuralNet(param_dict)
         self.model.load_state_dict(torch.load(model_path))
