d088f540c2fde09e1763258ac3cdc709c40f4927
kosiarn <kosiarn@users.noreply.github.com>
2023-05-04 13:40:40 +0200

    implement model parameter saving

--- services/backend_ml/configManager.py
+++ services/backend_ml/configManager.py
@@ -1,15 +1,16 @@
-
+import json
 
 def saveModel(name: str):
     raise NotImplementedError
 
+def saveModelParams(name: str, architecture_parameter_dict: dict):
+    with open(f"./usrModels/{name}.json", "+w") as param_file:
+        param_file.write(json.stringify(architecture_parameter_dict))
+
 def importModel(name: str):
     raise NotImplementedError
 
-def readModelConfig(file_name: str):
-    raise NotImplementedError
-
-def writeConfig(file_name: str):
+def importModelParams(name: str):
     raise NotImplementedError
 
 def flushModelMemoryToFile(file_name: str, loaded_models: dict):
