4ad7afdef69afe926d90521a5e4092bac04c87fe
kosiarn <kosiarn@users.noreply.github.com>
2023-05-05 11:47:44 +0200

    add todo

--- services/backend_ml/trainers.py
+++ services/backend_ml/trainers.py
@@ -1,6 +1,8 @@
 import baseModels as models
 import torch
 
+# TODO utilize model manager to import models
+
 class NeuralNetTrainer():
     def __init__(self, model_path: str, param_dict: dict):
         self.model = models.neuralNet(param_dict)
