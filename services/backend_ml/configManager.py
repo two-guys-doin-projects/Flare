d3e82a1b7ff7a96da2a53c67778b0533e9ee04bc
kosiarn <62908964+kosiarn@users.noreply.github.com>
2023-05-05 09:48:59 +0200

    add basic import logic

--- services/backend_ml/configManager.py
+++ services/backend_ml/configManager.py
@@ -12,11 +12,19 @@ def saveModelParams(name: str, architecture_parameter_dict: dict):
     with open(f"./usrModels/{name}.json", "+w") as param_file:
         param_file.write(json.dumps(architecture_parameter_dict))
 
+#TODO how to handle recreating different types of models?
+
 def importModel(name: str):
-    raise NotImplementedError
+    path = f"./usrModels/{name}"
+    if not os.path.exists(f"{path}.json"):
+        return 0
+
+#TODO test importing
 
 def importModelParams(name: str):
-    raise NotImplementedError
+    if not os.path.exists(f"./usrModels/{name}.json"):
+        raise FileNotFoundError
+    return json.load(f"./usrModels/{name}.json")
 
 def flushModelMemoryToFile(file_name: str, loaded_models: dict):
     raise NotImplementedError
