a52af4684fb2271060a07f2558d1619ad323eb0e
kosiarn <kosiarn@users.noreply.github.com>
2023-05-05 17:42:36 +0200

    rewrite trainer init

--- services/backend_ml/trainers.py
+++ services/backend_ml/trainers.py
@@ -1,9 +1,21 @@
-import baseModels as models
-import torch
+import torch.optim as optim
+import modelManager as manager
+import torch.nn as nn
 
-# TODO utilize model manager to import models
+# TODO implement trainer
+# in the most basic state it should just:
+# train the network, 
+# save the result model state dict
+# and give an option to save it to the model's file in ./usrModels/
+# if you're feeling fancy, make it:
+# do the above, but
+# make it save a checkpoint every epoch(or idk last three)
+# generate an accuracy report available under an API endpoint
+# and give an option to save a chosen chekpoint to the model's file
 
 class NeuralNetTrainer():
-    def __init__(self, model_path: str, param_dict: dict):
-        self.model = models.neuralNet(param_dict)
-        self.model.load_state_dict(torch.load(model_path))
+    def __init__(self, name: str):
+        self.model = manager.importModel(name)
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.SGD(self.model.parameters(), lr = 0.001, momentum = 0.9)
+
