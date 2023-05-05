dabd8d385f6ed8df34ff298283abcfc9808dc3a5
kosiarn <kosiarn@users.noreply.github.com>
2023-05-05 10:30:43 +0200

    add docs

--- services/backend_ml/configManager.py
+++ services/backend_ml/configManager.py
@@ -1,8 +1,16 @@
 import json
 import os
 import torch
+import torch.nn as nn
 
-def saveModel(name: str, model):
+def saveModel(name: str, model: nn.Module):
+    """
+    Zapisuje model na dysku.
+    # Parametry
+
+    - `name`: String - nazwa, pod jaką ma być zapisany model.
+    - `model`: torch.nn.Module(lub klasy dziedziczące) - model do zapisu. Musi posiadać słownik parametrów jako atrybut `params`
+    """
     path = f"./usrModels/{name}.pth"
     torch.save(model.state_dict(), path)
     if not os.path.exists(f"./usrModels/{name}.json"):
@@ -21,7 +29,10 @@ def importModel(name: str):
 
 #TODO test importing
 
-def importModelParams(name: str):
+def importModelParams(name: str) -> dict:
+    """
+    Zwraca słownik parametrów architektury wczytany z pliku konfiguracyjnego o podanej nazwie.
+    """
     if not os.path.exists(f"./usrModels/{name}.json"):
         raise FileNotFoundError
     return json.load(f"./usrModels/{name}.json")
@@ -29,7 +40,15 @@ def importModelParams(name: str):
 def flushModelMemoryToFile(file_name: str, loaded_models: dict):
     raise NotImplementedError
 
-def  deleteModel(name: str, truncate: bool = False):
+def  deleteModelFromDisk(name: str, truncate: bool = False):
+    """
+    Usuwa model z dysku. Nie ingeruje w załadowane modele.
+    # Parametry
+    - `name`: String - nazwa modelu.
+    - `truncate`: bool - ustawienie na `True` spowoduje, że poza usunięciem parametrów modelu
+
+    usunięta zostanie także konfiguracja architektury.
+    """
     os.remove(f"./usrModels/{name}.pth")
     if truncate:
         os.remove(f"./usrModels/{name}.json")
