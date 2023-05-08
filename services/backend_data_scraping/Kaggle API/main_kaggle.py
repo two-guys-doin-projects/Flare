89197641986fd1d7c95246e63ee6679a5b6e4778
Filip Walkowicz <fillio00@wp.pl>
2023-05-08 23:45:36 +0200

    add fastapi send/show dataset

--- services/backend_data_scraping/Kaggle API/main_kaggle.py
+++ services/backend_data_scraping/Kaggle API/main_kaggle.py
@@ -1,10 +1,10 @@
 import uvicorn
+import pandas as pd
 import init_kaggle
 import search_datasets_kaggle
 import download_dataset_kaggle
 import requests
 from fastapi import FastAPI, UploadFile
-
 
 
 app = FastAPI()
@@ -17,19 +17,38 @@ async def read_root():
 
 @app.get("/ping")
 def ping():
-    return {"message": "kozła!"}
+    return {"message": downloaded_dataset}
+
+
+@app.get("/get_dataset_name")
+def get_dataset_name(name: str):
+    return {"dataset_name": name}
+
+
+@app.get("/show_dataset")
+def show_dataset():
+    datasets = list_of_datasets.to_json()
+    return {"Lista zbiorów danych": datasets}
+
+
+@app.get("/send_dataset")
+def send_dataset():
+    datasets = downloaded_dataset.to_json()
+    return {"Lista zbiorów danych": datasets}
 
 
 if __name__ == "__main__":
     kaggle_api = init_kaggle.kaggle_api_authentication()
     # place holder
     dataset_name = "air condition"
-    list_of_datasets, list_of_files = search_datasets_kaggle.show_datasets(
+    list_of_datasets = search_datasets_kaggle.show_datasets(
         kaggle_api, dataset_name
     )
-    print(f"Lista zbiorów danych spod hasła {dataset_name}: \n {list_of_files}")
-    downloaded_dataset = download_dataset_kaggle.download_dataset(kaggle_api, list_of_files)
-    print(f"Pobierano: {list_of_files.pliki[0]} \n {downloaded_dataset}")
+    print(f"Lista zbiorów danych spod hasła {dataset_name}: \n {list_of_datasets}")
+    downloaded_dataset = download_dataset_kaggle.download_dataset(kaggle_api, list_of_datasets)
+    print(f"Pobierano: {list_of_datasets.pliki[0]} \n {downloaded_dataset}")
     uvicorn.run(app, host="localhost", port=8000)
-    #requests.post("localhost:8000/receive_df", data=downloaded_dataset.all)
+
+
+
 
