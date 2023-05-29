ac8633d7b70921e6f3db1de7e24652f7424151a1
kosiarn <kosiarn@users.noreply.github.com>
2023-05-30 01:40:24 +0200

    add model basic picker

---
+++ services/frontend/vue/src/components/modelPicker.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+const models = [
+    {name: 'neural1', type: 'neural'},
+    {name: 'neural2', type: 'neural'}
+]
+
+</script>
+
+<template>
+    <div class="panel">    
+        <div v-for="model in models" class="list-position">
+            <p>{{ model.name }}</p>
+            <p>{{ model.type }}</p>
+        </div>
+        <div class="btn add-button"> + </div>
+    </div>
+</template>
+
+<style scoped>
+.add-button{
+    float: right;
+    margin-right: 15px;
+    margin-bottom: 15px;
+    width: 54px;
+    height: 54px;
+    margin-top: 15px;
+    text-align: center;
+    vertical-align: middle;
+    line-height: 54px;
+}
+</style>
