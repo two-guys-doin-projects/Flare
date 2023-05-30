d1156141aa05fb6af7b9deecb4af4bff3ee89dde
kosiarn <kosiarn@users.noreply.github.com>
2023-05-30 03:13:09 +0200

    add upper bar model display

---
+++ services/frontend/vue/src/components/modelDisplay.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useModelStore } from './model';
+
+const store = useModelStore();
+
+</script>
+
+<template>
+    <div class="btn">
+    <h3>Model</h3>
+    <p>{{ store.name === "" ? "Wybierz model..." : store.name }}</p>
+</div>
+</template>
+
+<style scoped>
+.btn h3{
+    margin-left: 5px;
+}
+.btn p{
+    margin-left: 10px;
+    margin-top: -10px;
+}
+div{
+    height:100%;
+}
+</style>
