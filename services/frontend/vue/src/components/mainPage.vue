9b7e6a192f9a09045868b79934f76a3c618dd232
kosiarn <kosiarn@users.noreply.github.com>
2023-05-30 08:11:11 +0200

    add mainpage

---
+++ services/frontend/vue/src/components/mainPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<h1>Witaj w MultiLearning!</h1>
+<p>Zacznij od wybrania modelu, klikając na belkę u góry.</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
