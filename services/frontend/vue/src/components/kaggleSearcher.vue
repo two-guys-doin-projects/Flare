0902545569341f6ed09fc886ab0cc069f2f0073a
kosiarn <kosiarn@users.noreply.github.com>
2023-05-30 03:55:04 +0200

    add search page

---
+++ services/frontend/vue/src/components/kaggleSearcher.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useKaggleStore } from './kaggleStore';
+import {ref} from 'vue';
+
+const phrase = ref<String>();
+
+
+</script>
+
+<template>
+<div class="wrapper">
+    <input v-bind="phrase" />
+    <div class="btn search-btn"></div>
+</div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.search-btn{
+    margin-top: 20px;
+    width: 250px;
+    height: 50px
+}
+
+input{
+    margin-top: 10px;
+    width: 400px;
+    height: 30px;
+}
+</style>
