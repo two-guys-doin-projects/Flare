51ad337bba603019f55c995307100596989343cb
kosiarn <kosiarn@users.noreply.github.com>
2023-06-03 14:44:06 +0200

    add model listing selecting and updating endpoints

--- services/backend_ml/main.py
+++ services/backend_ml/main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import requests
 from contextlib import asynccontextmanager
+import modelManager as manager
 
 #dev flags
 
 enforce_cors = False
 
-model_storage = {}
+loaded_models = {}
+
+model_save_path = 'usrModels/'
 
 
 #lifecycle logic
@@ -16,7 +19,8 @@ model_storage = {}
 async def lifespan(app: FastAPI):
     print("Starting...")
     yield
-    print("Shutting down...")
+    print("Saving all loaded models...")
+    manager.flushModelMemoryToFile(loaded_models)
 
 
 app = FastAPI()
@@ -36,3 +40,21 @@ if enforce_cors:
 @app.get("/ping")
 def pong():
     return {"message": "pong!"}
+
+@app.get("/models/list")
+def getModelList():
+    return(manager.listAvailableModels(loaded_models, model_save_path))
+
+@app.post("/model/select", status_code=status.HTTP_200_OK)
+def ensureModelIsLoaded(model_name: str):
+    try:
+        loaded_models[model_name] = manager.importModel(model_name)
+    except FileNotFoundError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+@app.post("/model/create")
+def createModel(name: str, type: str, params: dict):
+    try:
+        loaded_models[name] = createModel(name, type, params)
+    except:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
