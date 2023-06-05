afe6629826d08b509d81af6ac94915062dab4109
kosiarn <kosiarn@users.noreply.github.com>
2023-06-05 09:15:01 +0200

    remove TODO

--- services/backend_ml/trainers.py
+++ services/backend_ml/trainers.py
@@ -4,16 +4,7 @@ import torch.nn as nn
 import torch as T
 from typing import List, Tuple
 
-# TODO implement trainer
-# in the most basic state it should just:
-# train the network, 
-# save the result model state dict
-# and give an option to save it to the model's file in ./usrModels/
-# if you're feeling fancy, make it:
-# do the above, but
-# make it save a checkpoint every epoch(or idk last three)
-# generate an accuracy report for every checkpoint available under an API endpoint
-# and give an option to save a chosen chekpoint to the model's file
+
 
 class NeuralNetTrainer():
     def __init__(self, name: str):
