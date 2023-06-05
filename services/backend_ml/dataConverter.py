05ab1a8a3bb54d9087cde4e8d128c9d3b3add513
kosiarn <kosiarn@users.noreply.github.com>
2023-06-05 09:13:47 +0200

    add json to dataset converter util

---
+++ services/backend_ml/dataConverter.py
@@ -0,0 +1,19 @@
+import json
+from torch import FloatTensor
+from typing import List
+
+def convertToDataset(data: str, eval_column_indices: List[int]):
+    dict_data: dict = json.loads(data)
+    dataset = []
+    columns = list(dict_data['train'][0].keys())
+    train_columns = []
+    eval_columns = []
+    for column_index in range(len(columns)):
+        (eval_columns.append(columns[column_index]) 
+            if column_index in eval_column_indices 
+            else train_columns.append(columns[column_index]))
+    for row in dict_data['train']:
+        inputs = [float(row[column]) for column in train_columns]
+        outputs = [float(row[column]) for column in eval_columns]
+        dataset.append((FloatTensor(inputs), FloatTensor(outputs)))
+    return dataset
