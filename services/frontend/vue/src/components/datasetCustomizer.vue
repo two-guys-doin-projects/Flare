99631b616a2d048c2117db6a8f585b5b8115277f
kosiarn <kosiarn@users.noreply.github.com>
2023-06-06 12:48:36 +0200

    add removing columns

--- services/frontend/vue/src/components/datasetCustomizer.vue
+++ services/frontend/vue/src/components/datasetCustomizer.vue
@@ -3,14 +3,20 @@ import { onMounted } from 'vue';
 import { ref } from 'vue';
 import { useKaggleStore } from './kaggleStore';
 import data from '../api/data_scraping';
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
 const store = useKaggleStore();
-
 const datasetHead = ref([{}]);
 const headings = ref()
 onMounted(() => {data().get(`/show_dataframe?index=${store.selected_dataset_index}`).then((response) => {datasetHead.value = response.data.head; headings.value = Object.keys(datasetHead.value[0])})})
 
+const markedForDeletion = ref([]);
 
+function thinTheDataset(){
+    data().get(`/selected_columns_of_dataframe?index=${String(markedForDeletion.value)}`)
+    router.push('mark_eval')
+}
 
 </script>
 
@@ -19,12 +25,16 @@ onMounted(() => {data().get(`/show_dataframe?index=${store.selected_dataset_inde
 <div v-if="datasetHead != null">
     <table>
         <tr>
-            <th v-for="(heading, index) in headings"> {{ heading }} </th>
+            <th v-for="(heading, index) in headings">
+                {{ heading }}
+                <input type="checkbox" v-bind:id='String(index)' v-bind:value='index' v-model="markedForDeletion"/>
+            </th>
         </tr>
         <tr v-for="row in datasetHead">
             <th v-for="entry in row"> {{ entry }} </th>
         </tr>
     </table>
+    <div class="btn add-button" @click="thinTheDataset"> Usuń zaznaczone kolumny... </div>
 </div>
 
 </template>
@@ -32,5 +42,16 @@ onMounted(() => {data().get(`/show_dataframe?index=${store.selected_dataset_inde
 <style scoped>
 table {
     background-color: lightgray;
+}
+.add-button{
+    float: right;
+    margin-right: 15px;
+    margin-bottom: 15px;
+    width: 216px;
+    height: 54px;
+    margin-top: 15px;
+    text-align: center;
+    vertical-align: middle;
+    line-height: 54px;
 }
 </style>
