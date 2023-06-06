eddf2b57b67db346e96510c39795024bd04fd06d
kosiarn <kosiarn@users.noreply.github.com>
2023-06-06 07:01:38 +0200

    apparently I receive a dict in this finc

--- services/backend_ml/dataConverter.py
+++ services/backend_ml/dataConverter.py
@@ -3,16 +3,16 @@ from torch import FloatTensor
 from typing import List
 
 def convertToDataset(data: str, eval_column_indices: List[int]):
-    dict_data: dict = json.loads(data)
+    #dict_data: dict = json.loads(data)
     dataset = []
-    columns = list(dict_data['train'][0].keys())
+    columns = list(data['train'][0].keys())
     train_columns = []
     eval_columns = []
     for column_index in range(len(columns)):
         (eval_columns.append(columns[column_index]) 
             if column_index in eval_column_indices 
             else train_columns.append(columns[column_index]))
-    for row in dict_data['train']:
+    for row in data['train']:
         inputs = [float(row[column]) for column in train_columns]
         outputs = [float(row[column]) for column in eval_columns]
         dataset.append((FloatTensor(inputs), FloatTensor(outputs)))
