825d2a313cc1be9fdc1f000e15f40c237fb3e003
fwalkowicz <walkowiczf@gmail.com>
2023-10-20 16:12:00 +0200

    Routing

--- services/frontend/src/App.jsx
+++ services/frontend/src/App.jsx
@@ -1,16 +1,31 @@
-import NavBar from "../components/NavBar/NavBar"
-import MainContent from '../components/MainContent/MainContent'
-import Footer from "../components/Footer/Footer"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "../components/NavBar/NavBar";
+import MainContent from '../components/MainContent/MainContent';
+import Footer from "../components/Footer/Footer";
+import ModelPage from "../components/MainContent/UserPage/ModelPage/ModelPage";
+import DatasetPage from "../components/MainContent/UserPage/DatasetPage/DatasetPage";
+import TrainingPage from "../components/MainContent/UserPage/TrainingPage/TrainingPage";
+import PredictionPage from "../components/MainContent/UserPage/PredictionPage/PredictionPage";
+import UserPage from "../components/MainContent/UserPage/UserPage";
 
 function App() {
-  
   return (
     <>
-      <NavBar />
-      <MainContent />
-      <Footer />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="/user" element={<UserPage />}>
+            <Route path="model" element={<ModelPage />} />
+            <Route path="dataset" element={<DatasetPage />} />
+            <Route path="training" element={<TrainingPage />} />
+            <Route path="prediction" element={<PredictionPage />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
