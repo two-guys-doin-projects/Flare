cce52f5032471d449e31d1f45419800f0388e8fa
fwalkowicz <walkowiczf@gmail.com>
2023-10-20 13:33:41 +0200

    Add new blank component MainContent

---
+++ services/frontend/components/MainContent/MainContent.jsx
@@ -0,0 +1,11 @@
+import './MainContent.css'
+
+function MainContent(){
+    return(
+        <div className="main--content">
+            <h1>asd</h1>
+        </div>
+    )
+}
+
+export default MainContent
