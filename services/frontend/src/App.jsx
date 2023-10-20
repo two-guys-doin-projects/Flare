8a91200930715b965a45a9d3d332a5e9f47c1943
fwalkowicz <walkowiczf@gmail.com>
2023-10-20 13:50:32 +0200

    Update App component

--- services/frontend/src/App.jsx
+++ services/frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import NavBar from "../components/NavBar/NavBar"
+import MainContent from '../components/MainContent/MainContent'
+import Footer from "../components/Footer/Footer"
 
 function App() {
   
-
   return (
     <>
       <NavBar />
+      <MainContent />
+      <Footer />
     </>
   )
 }
