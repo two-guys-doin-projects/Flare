d40bfe9bcef0f22838fe5bf1220570be27986245
kosiarn <kosiarn@users.noreply.github.com>
2023-11-28 14:11:02 +0100

    add descriptor documentation

--- services/backend_ml/engine/models/pluginSchematics/descriptors.py
+++ services/backend_ml/engine/models/pluginSchematics/descriptors.py
@@ -8,6 +8,12 @@ class Descriptor(ABC):
 
 class IntRange(Descriptor):
     def __init__(self, description: str, min: int, max: int):
+        """
+        Describes an integer range in which parameter's value has to be.
+        Examples:
+        - Dropout probability,
+        - Amount of neural net layers
+        """
         self.type = "ranged"
         self.description = description
         self.min = round(min),
@@ -15,15 +21,33 @@ class IntRange(Descriptor):
 
 class Text(Descriptor):
     def __init__(self, description: str):
+        """
+        Describes a text value.
+        Examples:
+        - Model name
+        """
         self.type = "string"
         self.description = description
 
 class Int(Descriptor):
     def __init__(self, description: str):
+        """
+        Describes a numeric value in integer format.
+        Examples:
+        - Amount of neurons in a layer
+        """
         self.type = "int"
         self.description = description
 
 class ChooseOne(Descriptor):
     def __init__(self, description: str, possible_values: list):
+        """
+        Describes a concrete list of values that a parameter can take.
+        Examples:
+        - model's backend architecture,
+        - convolutional kernel size,
+        - activation function used in neurons
+        """
+        self.description = description
         self.type = "choose_one"
         self.possible_values = possible_values
