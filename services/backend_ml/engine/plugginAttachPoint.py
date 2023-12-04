3f31a56afc64b83d74fd2c1c6e360d4f6c005480
kosiarn <kosiarn@users.noreply.github.com>
2023-12-04 23:42:20 +0100

    fix plugin attachpoint imports they were not working in API mode

--- services/backend_ml/engine/plugginAttachPoint.py
+++ services/backend_ml/engine/plugginAttachPoint.py
@@ -1,14 +1,13 @@
 from importlib import import_module
-
-module_paths = __import__('models').modules
+from .models import modules
 
 def get_pluggable_model_modules():
     """
     Imports pluggable models.
     """
-    module_names = [module_path.split('\\')[-1].split('.')[0] for module_path in module_paths]
+    module_names = [module_path.split('\\')[-1].split('.')[0] for module_path in modules]
     return [
-        import_module("models." + module_name, package='.') for module_name in module_names if module_name != "__init__"
+        import_module(".models." + module_name, package='engine') for module_name in module_names if module_name != "__init__"
     ]
 
 def get_pluggable_models():
