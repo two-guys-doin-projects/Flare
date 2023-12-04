e318e460fd8aa0d74f77d2401a8b731e8cbc4ed4
kosiarn <kosiarn@users.noreply.github.com>
2023-12-04 23:41:51 +0100

    implement basic API attaches plugins to API and shares model details endpoints

--- services/backend_ml/main.py
+++ services/backend_ml/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-
+from engine.plugginAttachPoint import get_pluggable_models
 #dev flags
 
 DS_BACKEND_ADRESS = 'http://192.168.1.81:8081'
 
 enforce_cors = True
 
-
+available_architectures = get_pluggable_models()
 
 #lifecycle logic
 
@@ -16,7 +16,7 @@ enforce_cors = True
 async def lifespan(app: FastAPI):
     print("Starting...")
     yield
-    print("Saving all loaded models...")
+    print("Shutting down...")
 
 
 app = FastAPI()
@@ -32,3 +32,12 @@ if enforce_cors:
         allow_headers=["*"],
     )
 
+@app.get("/model/list")
+def list_available_models():
+    return [
+        model_name for model_name in available_architectures
+    ]
+
+@app.get("/model/create")
+def get_architecture_params(model_name: str):
+    return available_architectures[model_name].PluggableModel().get_param_object()
