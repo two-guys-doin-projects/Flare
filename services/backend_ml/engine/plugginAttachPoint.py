b788d899e15555f594e8a28eb5ded3efb0c10ae7
kosiarn <kosiarn@users.noreply.github.com>
2023-12-05 00:29:32 +0100

    fix readability in get_pluggable_model_modules()

--- services/backend_ml/engine/plugginAttachPoint.py
+++ services/backend_ml/engine/plugginAttachPoint.py
@@ -5,7 +5,9 @@ def get_pluggable_model_modules():
     """
     Imports pluggable models.
     """
-    module_names = [module_path.split('\\')[-1].split('/')[-1].split('.')[0] for module_path in modules]
+    list_last_element_index = -1
+    module_names_with_extensions = [module_path.split('\\')[list_last_element_index].split('/')[list_last_element_index] for module_path in modules]
+    module_names = [module_name.split('.')[0] for module_name in module_names_with_extensions]
     return [
         import_module(".models." + module_name, package='engine') for module_name in module_names if module_name != "__init__"
     ]
