c0105f22d07ddca1e57a10eeaf2896af9fe85981
fwalkowicz <walkowiczf@gmail.com>
2024-01-05 18:29:51 +0100

    update APP

--- services/frontend/src/App.jsx
+++ services/frontend/src/App.jsx
@@ -1,7 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./views/Home";
+import Models from "./views/Models";
+import Dataset from "./views/Dataset";
+import Train from "./views/Train";
+import Prediction from "./views/Prediction";
+import Footer from "./components/Footer";
+
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+            <Route path="/models" element={<Models />} />
+            <Route path="/dataset" element={<Dataset />} />
+            <Route path="/train" element={<Train />} />
+            <Route path="/predict" element={<Prediction />} />
+         
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   )
 }
